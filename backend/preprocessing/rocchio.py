655e9539294096db4a13bcdada86a0695ee1daab
Shreehari Srinivasan <ss2732@cornell.edu>
2024-04-26 00:11:25 -0400

    Added rocchio to frontend
    Co-authored-by: Ashwin Char <ashwinchar@users.noreply.github.com>

--- backend/preprocessing/rocchio.py
+++ backend/preprocessing/rocchio.py
@@ -1,20 +1,30 @@
 import numpy as np
 import pickle
 import json
-from tfidf import tfidf_matrix
+import os
+
+
+abspath = os.path.abspath(__file__)
+
+svd_path = os.path.join(os.path.dirname(abspath), "svd.sav")
+index_to_json_path = os.path.join(os.path.dirname(abspath), "index_to_doc.json")
+vectorizer_path = os.path.join(os.path.dirname(abspath), "vectorizer.sav")
+tfidf_matrix = np.load(os.path.join(os.path.dirname(abspath), "tfidf_matrix.npy"))
 
 
 def rocchio(query, relevant_docs, non_relevant_docs, alpha=1, beta=0.75, gamma=0.15):
-    vectorizer = pickle.load(open("vectorizer.sav", "rb"))
-    query_vec = vectorizer.transform([query]).toarray()
 
-    with open("index_to_doc.json") as file:
+    # generate doc_to_index
+    with open(index_to_json_path) as file:
         index_to_doc = json.load(file)
+    doc_to_index = {v: k for k, v in index_to_doc.items()}
+    vectorizer = pickle.load(open(vectorizer_path, "rb"))
+    query_vec = vectorizer.transform([query]).toarray()
 
     relevant_doc_matrix = None
 
     for brand_name in relevant_docs:
-        doc_id = index_to_doc[brand_name]
+        doc_id = int(doc_to_index[brand_name])
         doc_vec = tfidf_matrix[doc_id]
         if relevant_doc_matrix is None:
             relevant_doc_matrix = doc_vec
@@ -24,7 +34,7 @@ def rocchio(query, relevant_docs, non_relevant_docs, alpha=1, beta=0.75, gamma=0
     non_relevant_doc_matrix = None
 
     for brand_name in non_relevant_docs:
-        doc_id = index_to_doc[brand_name]
+        doc_id = int(doc_to_index[brand_name])
         doc_vec = tfidf_matrix[doc_id]
         if non_relevant_doc_matrix is None:
             non_relevant_doc_matrix = doc_vec
